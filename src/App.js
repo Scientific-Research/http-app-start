912dc56adec2ab5b95c7692a0e0bf65819c359a7
Maximilian <scientificresearch309@gmail.com>
2023-08-22 22:00:10 +0200

    Getting Data and showing them on Browser!

--- src/App.js
+++ src/App.js
@@ -1,64 +1,79 @@
-import React, { Component } from "react";
-import "./App.css";
+import React, { Component } from 'react';
+import axios from 'axios';
+import './App.css';
 
 class App extends Component {
-  state = {
-    posts: []
-  };
+	state = {
+		posts: [],
+	};
 
-  handleAdd = () => {
-    console.log("Add");
-  };
+	async componentDidMount() {
+		//pending > resolved (success) OR rejected (failure)
+		// const promise = axios.get('https://jsonplaceholder.typicode.com/posts');
+		const { data: posts } = await axios.get(
+			'https://jsonplaceholder.typicode.com/posts'
+		);
 
-  handleUpdate = post => {
-    console.log("Update", post);
-  };
+		// console.log(promise);
+		// const response = await promise;
+		console.log('posts in axios: ', posts);
+		this.setState({ posts: posts });
+		console.log('posts in state(after this.setState): ', posts);
+	}
 
-  handleDelete = post => {
-    console.log("Delete", post);
-  };
+	handleAdd = () => {
+		console.log('Add');
+	};
 
-  render() {
-    return (
-      <React.Fragment>
-        <button className="btn btn-primary" onClick={this.handleAdd}>
-          Add
-        </button>
-        <table className="table">
-          <thead>
-            <tr>
-              <th>Title</th>
-              <th>Update</th>
-              <th>Delete</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.posts.map(post => (
-              <tr key={post.id}>
-                <td>{post.title}</td>
-                <td>
-                  <button
-                    className="btn btn-info btn-sm"
-                    onClick={() => this.handleUpdate(post)}
-                  >
-                    Update
-                  </button>
-                </td>
-                <td>
-                  <button
-                    className="btn btn-danger btn-sm"
-                    onClick={() => this.handleDelete(post)}
-                  >
-                    Delete
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </React.Fragment>
-    );
-  }
+	handleUpdate = (post) => {
+		console.log('Update', post);
+	};
+
+	handleDelete = (post) => {
+		console.log('Delete', post);
+	};
+
+	render() {
+		return (
+			<React.Fragment>
+				<button className="btn btn-primary" onClick={this.handleAdd}>
+					Add
+				</button>
+				<table className="table">
+					<thead>
+						<tr>
+							<th>Title</th>
+							<th>Update</th>
+							<th>Delete</th>
+						</tr>
+					</thead>
+					<tbody>
+						{this.state.posts.map((post) => (
+							<tr key={post.id}>
+								<td>{post.title}</td>
+								<td>
+									<button
+										className="btn btn-info btn-sm"
+										onClick={() => this.handleUpdate(post)}
+									>
+										Update
+									</button>
+								</td>
+								<td>
+									<button
+										className="btn btn-danger btn-sm"
+										onClick={() => this.handleDelete(post)}
+									>
+										Delete
+									</button>
+								</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
+			</React.Fragment>
+		);
+	}
 }
 
 export default App;
